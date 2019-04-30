67ec74e0b9a78c5882723d1729831b81fd8bfd44
Paul Solt <PaulSolt@gmail.com>
2019-04-30 13:31:34 -0400

    Added playground and updated UI logic.

--- iOS6Stars/StarController.swift
+++ iOS6Stars/StarController.swift
@@ -11,13 +11,26 @@ import Foundation
 class StarController {
     
     // name, distance
+    // addStar(named: "Sun", withDistance: 0.0)
     func addStar(named name: String, withDistance distance: Double) {
         
         // 1. Create a star
         // 2. Add the star to the list
+        let star = Star(name: name, distance: distance)
+        stars.append(star)
     }
     
+    // Save to disk
+    func saveToPerisitentStore() {
+        
+    }
     
+    // Load from the disk
+    func loadFromPersistentStore() {
+        
+    }
+    
+    // Encapsulation: protect our data from (external) modification
     // Sun, Alpha Centauri, etc. ...
     private(set) var stars: [Star] = []
 }
--- iOS6Stars/ViewController.swift
+++ iOS6Stars/ViewController.swift
@@ -12,12 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // starController.stars = [Star(name: "Sun", distance: 0.0)]
     }
 
     @IBAction func addStarButtonPressed(_ sender: Any) {
-        print("add star")
+        // name (String), distance (Double)
+        // Text Field text is always a String
+        guard let name = nameTextField.text,
+            let distanceString = distanceTextField.text,
+            let distance = Double(distanceString) else {
+                print("Invalid input!")
+                return
+        }
+        // We only get down here when name is not nil, or we have a valid number
+        //print("name: \(name) distance: \(distance)")
+        starController.addStar(named: name, withDistance: distance)
+        listStars()
     }
+    
+    // list all of the stars we're tracking
+    
+    func listStars() {
+        // 1. Create a string
+        // 2. Append text about each star
+        // 3. Display it in the textview
+        
+        var output = ""
+        
+        for star in starController.stars {
+            output += "\(star.name) is \(star.distance) light years away.\n"
+        }
+        textView.text = output
+    }
+    
+    
+    let starController = StarController()
     
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var distanceTextField: UITextField!
